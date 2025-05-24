4b499964c39f96d47ae070da7fd98443679732b2
Alfredo Pérez <index@mailbox.org>
2025-05-23 21:13:15 -0500

    clint: add mtime and mtimecmp accessors

--- inc/fe310/clint.h
+++ inc/fe310/clint.h
@@ -12,4 +12,38 @@ extern volatile u32 *const mtimecmp_hi;
 extern volatile u32 *const mtime_lo;
 extern volatile u32 *const mtime_hi;
 
+static inline void
+clint_set_mtime(u64 time)
+{
+	*mtime_hi = (u32)(time >> 32);
+	*mtime_lo = (u32)time;
+}
+
+static inline u64
+clint_get_mtime(void)
+{
+	u32 hi, lo;
+
+	do {
+		hi = *mtime_hi;
+		lo = *mtime_lo;
+	} while (*mtime_hi != hi);
+
+	return lo | (((u64)hi) << 32);
+}
+
+static inline void
+clint_set_mtimecmp(u64 time)
+{
+	*mtimecmp_hi = UINT32_MAX;
+	*mtimecmp_lo = time;
+	*mtimecmp_hi = (u32)(time >> 32);
+}
+
+static inline u64
+clint_get_mtimecmp(void)
+{
+	return *mtimecmp_lo | (((u64)*mtimecmp_hi) << 32);
+}
+
 #endif /* LIBFE310_CLINT_H */
