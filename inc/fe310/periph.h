251501765863c182ae2b5dd93640e303dee3b590
Alfredo Pérez <index@mailbox.org>
2025-06-01 08:02:46 -0500

    periph: rename wm enums to match fifo threshold semantics

--- inc/fe310/periph.h
+++ inc/fe310/periph.h
@@ -4,14 +4,14 @@
 #define LIBFE310_PERIPH_H
 
 typedef enum {
-	WM_1 = 0,
-	WM_2 = 1,
-	WM_3 = 2,
-	WM_4 = 3,
-	WM_5 = 4,
-	WM_6 = 5,
-	WM_7 = 6,
-	WM_8 = 7,
+	WM_0 = 0,
+	WM_1 = 1,
+	WM_2 = 2,
+	WM_3 = 3,
+	WM_4 = 4,
+	WM_5 = 5,
+	WM_6 = 6,
+	WM_7 = 7,
 } WaterMarkLevel;
 
 #endif /* LIBFE310_PERIPH_H */
