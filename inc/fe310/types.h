d7bddc003814855b0f60338388b12b5356f3f328
Alfredo Pérez <index@mailbox.org>
2025-07-18 12:00:22 -0600

    types: replace built-in typedefs with freestanding std headers

--- inc/fe310/types.h
+++ inc/fe310/types.h
@@ -3,22 +3,20 @@
 #ifndef LIBFE310_TYPES_H
 #define LIBFE310_TYPES_H
 
-typedef __INT8_TYPE__ s8;
-typedef __INT16_TYPE__ s16;
-typedef __INT32_TYPE__ s32;
-typedef __INT64_TYPE__ s64;
-typedef __UINT8_TYPE__ u8;
-typedef __UINT16_TYPE__ u16;
-typedef __UINT32_TYPE__ u32;
-typedef __UINT64_TYPE__ u64;
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-typedef __SIZE_TYPE__ usize;
-typedef __PTRDIFF_TYPE__ ssize;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-#ifndef __cplusplus
-typedef _Bool bool;
-#define true 1
-#define false 0
-#endif
+typedef size_t usize;
+typedef ptrdiff_t ssize;
 
 #endif /* LIBFE310_TYPES_H */
